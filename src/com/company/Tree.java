11fb9f997a71adc3641e9f75cb0038b4bf617e4d
trolund@gmail.com <vxh77kdp>
2018-12-13 22:28:23 +0100

    bug!!

---
+++ src/com/company/Tree.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree<T> {
+    private Node<T> root;
+
+    public Tree(T rootData) {
+        root = new Node<T>();
+        root.data = rootData;
+        root.children = new ArrayList<Node<T>>();
+    }
+
+    public Node<T> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<T> root) {
+        this.root = root;
+    }
+
+    public static class Node<T> {
+        private T data;
+        private Node<T> parent;
+        private ArrayList<Node<T>> children = new  ArrayList();
+
+        public Node(T data, Node<T> parent, ArrayList<Node<T>> children) {
+            this.data = data;
+            this.parent = parent;
+            this.children = children;
+        }
+
+        public Node() {
+            this.data = data;
+            this.parent = parent;
+            this.children = children;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getParent() {
+            return parent;
+        }
+
+        public void setParent(Node<T> parent) {
+            this.parent = parent;
+        }
+
+        public ArrayList<Node<T>> getChildren() {
+            return children;
+        }
+
+        public void setChildren(ArrayList<Node<T>> children) {
+            this.children = children;
+        }
+    }
+}
+
---
+++ src/com/company/impurityGain.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.List;
+
+public class impurityGain {
+
+
+    public static void main(String[] args) {
+
+        // build tree
+        Tree.Node<Object> rootNode = new Tree.Node<>();
+
+        Tree.Node<Object> nodeclass1 = new Tree.Node<>();
+        Tree.Node<Object> nodeclass2 = new Tree.Node<>();
+
+        // connect tree
+        rootNode.getChildren().add(nodeclass1);
+        rootNode.getChildren().add(nodeclass2);
+
+        Tree tree = new Tree(rootNode);
+        
+
+
+
+    }
+
+}
