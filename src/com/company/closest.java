b0d07ff8a2209a91e6844fb4307f70ce435c9d60
trolund@gmail.com <vxh77kdp>
2018-12-13 21:32:59 +0100

    bug!!

---
+++ src/com/company/closest.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class closest {
+
+    private static double[] clustrevalues = {4,7,14};
+
+    private static double[] xValues = {3,6,7,10,11,14};
+
+    private static ArrayList<ArrayList<Object>> clustre = new ArrayList<>();
+
+
+
+    public static void main(String[] args) {
+
+        for (Double cluster: clustrevalues) {
+
+        }
+
+
+        for(int x = 0; x < xValues.length; x++){
+            for(int y = 0; x < clustrevalues.length; x++){
+
+
+
+            }
+
+        }
+
+    }
+}
+
