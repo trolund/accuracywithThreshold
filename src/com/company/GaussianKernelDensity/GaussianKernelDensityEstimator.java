bb267e344882272a90269bd35ca4ebe19a41fb2d
iyyel <i@iyyel.io>
2018-12-13 22:44:01 +0100

    Refactoring package structure.

---
+++ src/com/company/GaussianKernelDensity/GaussianKernelDensityEstimator.java
@@ -0,0 +1,28 @@
+package com.company.GaussianKernelDensity;
+
+public class GaussianKernelDensityEstimator {
+
+    private static double[] dist = {4.39, 3.62, 4.53, 3.83, 4.36, 4.14, 4.06, 4.31};
+
+    private static double M = 6;
+    private static double variance = 1;
+    private static double N = 8;
+
+    public static void main(String[] args) {
+
+        double sum = 0;
+
+        for (double dist : dist) {
+            sum += calc(dist);
+        }
+
+        double res = (sum * (1 / (Math.pow(2 * Math.PI, M / 2) * Math.sqrt(Math.pow(variance, M * 2))))) / N;
+
+        System.out.println(res);
+    }
+
+    private static double calc(double val) {
+        return Math.exp(-(Math.pow(val, 2) / 2));
+    }
+
+}
